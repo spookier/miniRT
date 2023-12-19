ec5310be5b4d2d895286637736ca2627472dc72d
sleepychloe <hyj2132@gmail.com>
2023-12-19 12:32:01 +0100

    20231219_12:31 | fixed_camera_rotation

--- execute/srcs/key_hooks.c
+++ execute/srcs/key_hooks.c
@@ -6,7 +6,7 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/09 21:48:34 by acostin           #+#    #+#             */
-/*   Updated: 2023/12/19 03:50:32 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/12/19 12:31:01 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,35 +16,35 @@ int	key_hook(int keycode, t_all *all)
 {
 	if (keycode == KEY_ESC)
 		exit_prog(all, NULL);
-	if (keycode == KEY_1)
-		all->cam.direction.x -= 0.2;
-	if (keycode == KEY_2)
-		all->cam.direction.x += 0.2;
-	if (keycode == KEY_3)
-		all->cam.direction.y -= 0.2;
-	if (keycode == KEY_4)
-		all->cam.direction.y += 0.2;
-	if (keycode == KEY_5)
-		all->cam.direction.z -= 0.2;
-	if (keycode == KEY_6)
-		all->cam.direction.z += 0.2;
-	if (keycode == KEY_Q)
-		all->cam.origin.x -= 1;
-	if (keycode == KEY_W)
-		all->cam.origin.x += 1;
-	if (keycode == KEY_E)
-		all->cam.origin.y -= 1;
-	if (keycode == KEY_R)
-		all->cam.origin.y += 1;
-	if (keycode == KEY_T)
-		all->cam.origin.z -= 1;
-	if (keycode == KEY_Y)
-		all->cam.origin.z += 1;
-	if (keycode != KEY_ESC)
-	{
-		start_draw(all);
-		mlx_put_image_to_window(all->g_eng.mlx,
-			all->g_eng.mlx_win, all->img.img, 0, 0);
-	}
+	// if (keycode == KEY_1)
+	// 	all->cam.direction.x -= 0.2;
+	// if (keycode == KEY_2)
+	// 	all->cam.direction.x += 0.2;
+	// if (keycode == KEY_3)
+	// 	all->cam.direction.y -= 0.2;
+	// if (keycode == KEY_4)
+	// 	all->cam.direction.y += 0.2;
+	// if (keycode == KEY_5)
+	// 	all->cam.direction.z -= 0.2;
+	// if (keycode == KEY_6)
+	// 	all->cam.direction.z += 0.2;
+	// if (keycode == KEY_Q)
+	// 	all->cam.origin.x -= 1;
+	// if (keycode == KEY_W)
+	// 	all->cam.origin.x += 1;
+	// if (keycode == KEY_E)
+	// 	all->cam.origin.y -= 1;
+	// if (keycode == KEY_R)
+	// 	all->cam.origin.y += 1;
+	// if (keycode == KEY_T)
+	// 	all->cam.origin.z -= 1;
+	// if (keycode == KEY_Y)
+	// 	all->cam.origin.z += 1;
+	// if (keycode != KEY_ESC)
+	// {
+	// 	start_draw(all);
+	// 	mlx_put_image_to_window(all->g_eng.mlx,
+	// 		all->g_eng.mlx_win, all->img.img, 0, 0);
+	// }
 	return (0);
 }
--- execute/srcs/set_cam_vec.c
+++ execute/srcs/set_cam_vec.c
@@ -6,23 +6,16 @@
 /*   By: yhwang <yhwang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/19 08:54:39 by yhwang            #+#    #+#             */
-/*   Updated: 2023/12/19 10:07:08 by yhwang           ###   ########.fr       */
+/*   Updated: 2023/12/19 12:28:25 by yhwang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/minirt.h"
 
-void	cam_vec_0_0_1(t_scene *scene, t_camera *cam)
+void	rotate_scene(t_scene *scene, t_camera *cam, t_matrix3 m)
 {
-	double		val_cos;
-	double		val_sin;
-	t_matrix3	m;
-	int			i;
-
-	val_cos = cos(PI);
-	val_sin = sin(PI);
-	m = matrix3(vec3(val_cos, 0, -1 * val_sin),
-			vec3(0, 1, 0), vec3(val_sin, 0, val_cos));
+	int	i;
+	
 	i = -1;
 	while (++i < scene->num_spheres + scene->num_planes + scene->num_cylinder)
 	{
@@ -35,9 +28,66 @@ void	cam_vec_0_0_1(t_scene *scene, t_camera *cam)
 	cam->origin = matrix_mul(m, cam->origin);
 }
 
+void	rotate_y_axis(t_scene *scene, t_camera *cam)
+{
+	t_matrix3	m;
+
+	if (cam->direction.x == 0
+		&& cam->direction.y == 0 && cam->direction.z == 1)
+		m = matrix3(vec3(cos(PI), 0, -1 * sin(PI)),
+			vec3(0, 1, 0), vec3(sin(PI), 0, cos(PI)));
+	else if (cam->direction.x == -1
+		&& cam->direction.y == 0 && cam->direction.z == 0)
+		m = matrix3(vec3(cos(PI / 2), 0, -1 * sin(PI / 2)),
+			vec3(0, 1, 0), vec3(sin(PI / 2), 0, cos(PI / 2)));
+	else if (cam->direction.x == 1
+		&& cam->direction.y == 0 && cam->direction.z == 0)
+		m = matrix3(vec3(cos(3 * PI / 2), 0, -1 * sin(3 * PI / 2)),
+			vec3(0, 1, 0), vec3(sin(3 * PI / 2), 0, cos(3 * PI / 2)));
+	rotate_scene(scene, cam, m);
+}
+
+void	rotate_x_axis(t_scene *scene, t_camera *cam)
+{
+	t_matrix3	m;
+
+	if (cam->direction.x == 0
+		&& cam->direction.y == -1 && cam->direction.z == 0)
+		m = matrix3(vec3(1, 0, 0), vec3(0, cos(3 * PI / 2), sin(3 * PI / 2)),
+			vec3(0, -1 * sin(3 * PI / 2), cos(3 * PI / 2)));
+	else if (cam->direction.x == 0
+		&& cam->direction.y == 1 && cam->direction.z == 0)
+		m = matrix3(vec3(1, 0, 0), vec3(0, cos(PI / 2), sin(PI / 2)),
+			vec3(0, -1 * sin(PI / 2), cos(PI / 2)));
+	rotate_scene(scene, cam, m);
+}
+
+// void	rotate_x_y_axis(t_scene *scene, t_camera *cam)
+// {
+// 	t_matrix3	m;
+// 	float		val_cos;
+// 	float		val_sin;
+
+// 	val_cos = 
+// }
+
 void	set_cam_vec(t_all *all)
 {
 	if (all->cam.direction.x == 0
 		&& all->cam.direction.y == 0 && all->cam.direction.z == 1)
-		cam_vec_0_0_1(&all->scene, &all->cam);
+		rotate_y_axis(&all->scene, &all->cam);
+	else if (all->cam.direction.x == -1
+		&& all->cam.direction.y == 0 && all->cam.direction.z == 0)
+		rotate_y_axis(&all->scene, &all->cam);
+	else if (all->cam.direction.x == 1
+		&& all->cam.direction.y == 0 && all->cam.direction.z == 0)
+		rotate_y_axis(&all->scene, &all->cam);
+	else if (all->cam.direction.x == 0
+		&& all->cam.direction.y == -1 && all->cam.direction.z == 0)
+		rotate_x_axis(&all->scene, &all->cam);
+	else if (all->cam.direction.x == 0
+		&& all->cam.direction.y == 1 && all->cam.direction.z == 0)
+		rotate_x_axis(&all->scene, &all->cam);
+// 	else
+// 		rotate_x_y_axis(&all->scene, &all->cam);
 }
